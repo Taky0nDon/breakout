3fa7152d4ef77bc08d3892bdaaff0c42be90e79f
Taky0ndon <mmaggio89@gmail.com>
2024-09-22 19:33:06 -0400

    crated function setup_paddle_control to initiate listening for keypresses to move paddle

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ from Paddle import Paddle
 def setup(board: Board):
     right_edge = board.width / 2
     upper_edge = board.height / 2
-    board.screen.listen()
-    board.screen.onkey(fun=quit, key="q")
     pen = t.Turtle()
     pen.hideturtle()
     pen.penup()
@@ -16,9 +14,13 @@ def setup(board: Board):
     pen.write("Press q to quit")
     pen.penup()
     
+def setup_paddle_control(paddle: Paddle):
+    board.screen.onkey(fun=paddle.move_right, key="l")
+    board.screen.onkey(fun=paddle.move_left, key="h")
 board = Board()
 setup(board)
 
 paddle = Paddle(start_x=0, start_y=board.bottom)
+setup_paddle_control(paddle)
 
 board.screen.exitonclick()
