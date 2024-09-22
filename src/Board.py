2d8f807d70441350484f947419327408a2597fb4
Taky0ndon <mmaggio89@gmail.com>
2024-09-22 19:32:33 -0400

    put keypress for quit in Board class

--- src/Board.py
+++ src/Board.py
@@ -12,5 +12,9 @@ class Board():
         self.right_edge = self.width/2 * MARGIN
         self.left_edge = self.right_edge * -1
         self.bottom = self.height/-2*BOTTOM_MARGIN
+        self.start_listening_for_keys()
 
+    def start_listening_for_keys(self):
+        self.screen.listen()
+        self.screen.onkey(fun=quit, key="q")
 
