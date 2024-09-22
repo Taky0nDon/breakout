658a44315702134c043500c2b7ba4b145a17d253
Taky0ndon <mmaggio89@gmail.com>
2024-09-22 19:32:06 -0400

    added methods for moving paddle left and right

--- src/Paddle.py
+++ src/Paddle.py
@@ -5,6 +5,18 @@ class Paddle(t.Turtle):
     def __init__(self, start_x: int|float , start_y: int|float):
         super().__init__()
         self.shape("square")
-        self.shapesize(0.5, 2)
+        self.shapesize(0.5, 8)
         self.penup()
         self.goto(start_x, start_y)
+
+    def move_right(self):
+        current_x, current_y = self.pos()
+        move_distance = 10
+        new_x = current_x + move_distance
+        self.goto(new_x, current_y)
+
+    def move_left(self):
+        current_x, current_y = self.pos()
+        move_distance = -10
+        new_x = current_x + move_distance
+        self.goto(new_x, current_y)
